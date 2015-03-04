e0087505e14a683660559bb3a87bb3b53f61ebfa
chen-fang <chen-fang@utusla.edu>
2015-03-04 11:59:06 -0600

    ...

---
+++ Test/inline/020r05.cpp
@@ -0,0 +1,29 @@
+#include "../../Array/ADscalar.hpp"
+
+
+int main()
+{
+   ADscalar a, b, c, d, e;
+   ADscalar z;
+
+   a.make_independent(0);
+   b.make_independent(1);
+   c.make_independent(2);
+   d.make_independent(3);
+   e.make_independent(4);
+
+
+   std::size_t REPEAT = 1;//1E+06;
+   for( std::size_t iter = 0; iter < REPEAT; ++iter )
+   {
+      /* ADscalar with expression template */
+      z =a + b + c + d + e + 
+	 a + b + c + d + e + 
+	 a + b + c + d + e + 
+	 a + b + c + d + e ;
+
+   }
+
+   std::cout << z.value() << std::endl;
+
+}
